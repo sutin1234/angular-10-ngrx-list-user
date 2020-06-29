2c57b3ded7571a6e9669e6d9da79fcc9339d592c
sutin <sutin.i@quo-global.com>
2020-06-29 21:49:00 +0700

    added debug reducer

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -3,21 +3,27 @@ import {
   ActionReducerMap,
   createFeatureSelector,
   createSelector,
-  MetaReducer
+  MetaReducer,
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
 import * as fromUser from '../user.reducer';
 
-
 export interface State {
-
   [fromUser.userFeatureKey]: fromUser.State;
 }
 
 export const reducers: ActionReducerMap<State> = {
-
   [fromUser.userFeatureKey]: fromUser.reducer,
 };
+export function debug(reducer: ActionReducer<any>): ActionReducer<any> {
+  return (state, action) => {
+    console.log('state', state);
+    console.log('action', action);
 
+    return reducer(state, action);
+  };
+}
 
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<State>[] = !environment.production
+  ? [debug]
+  : [];
